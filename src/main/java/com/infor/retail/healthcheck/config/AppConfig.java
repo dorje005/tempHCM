000976d369c6d4792c9e02464323cb51e0610474
Ongda Dorjee <Ongda.Dorjee@infor.com>
2016-03-31 14:29:51 -0500

    Remove annotation from AppConfig class

--- src/main/java/com/infor/retail/healthcheck/config/AppConfig.java
+++ src/main/java/com/infor/retail/healthcheck/config/AppConfig.java
@@ -1,16 +1,13 @@
 package com.infor.retail.healthcheck.config;
 
 import com.infor.retail.healthcheck.model.Service;
-import com.infor.retail.healthcheck.service.AmazonS3Service.AccessS3;
 import com.infor.retail.healthcheck.service.HealthMonitorService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-@PropertySource("classpath:endpoint.properties")
 @Configuration
 public class AppConfig {
 
@@ -22,16 +19,16 @@ public class AppConfig {
         HealthMonitorService hms = new HealthMonitorService();
 
 //      read endpoints from config file stored in Amazon S3
-        AccessS3 accessS3 = new AccessS3();
-        String endpoints = accessS3.s3reader();
-        for (String entry : endpoints.split(" ;")) { // each service separated by ";"
-            String[] item = entry.split("="); // each service separated by "="
-            String serviceName = item[0];
-            String serviceURL = item[1];
-            int responseCode = hms.getStatusCode(serviceURL);
-            String subservice = hms.getSubService();
-            health_services.add(new Service(serviceName, responseCode, subservice));
-        }
+//        AccessS3 accessS3 = new AccessS3();
+//        String endpoints = accessS3.s3reader();
+//        for (String entry : endpoints.split(" ;")) { // each service separated by ";"
+//            String[] item = entry.split("="); // each service separated by "="
+//            String serviceName = item[0];
+//            String serviceURL = item[1];
+//            int responseCode = hms.getStatusCode(serviceURL);
+//            String subservice = hms.getSubService();
+//            health_services.add(new Service(serviceName, responseCode, subservice));
+//        }
         return health_services;
     }
 }
